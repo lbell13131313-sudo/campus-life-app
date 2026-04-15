96cc554909e882cdb7625f7c69365b3258c8815c
lbell13131313-sudo <lbell13131313@gmail.com>
2026-04-14 23:09:28 -0400

    Fixed up all the html pages, incorporated js with the button (will add more later), fixed the pages so that they would properly connect to style.css

--- js/script.js
+++ js/script.js
@@ -1,8 +1,4 @@
-//function goHome() { // sends the user back to the home page
-    //window.location.href = "home.html"; 
-//}
-
-// button for the home page
-//const homeButton = document.getElementById("#home-btn"); 
-
-//homeButton.addEventListener("click", goHome);
+// button for sending the user back to the home page
+document.getElementById("home-btn").onclick = function() {
+    window.location.href = "home.html";
+};
