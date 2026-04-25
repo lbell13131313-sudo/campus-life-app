6694328045a168f94235979941432ca42c846f96
lbell13131313-sudo <lbell13131313@gmail.com>
2026-04-25 11:05:04 -0400

    comments

--- js/script.js
+++ js/script.js
@@ -7,9 +7,10 @@ document.getElementById("home-btn").onclick = function() {
 async function getWeather() {
     const apiKey = '1ddbc56188878aee463d4182301720e8';
     const city = "hackettstown, new jersey";
-    const url = `http://api.weatherstack.com/current?access_key=${apiKey}&query=${city}`;
-
-    // http://weatherstack.com{apiKey}&query=${city}
+    // changes the units to fahrenheit instead of celsius
+    const units = "f";
+    // the url that the weather information will be grabbed from
+    const url = `http://api.weatherstack.com/current?access_key=${apiKey}&query=${city}&units=${units}`;
 
     try {
         const response = await fetch(url);
@@ -20,10 +21,10 @@ async function getWeather() {
             return;
         }
 
-        // Display results
+        // Displays the results on the events page
         document.getElementById('weatherDisplay').innerHTML = `
             <h3>${data.location.name}, ${data.location.country}</h3>
-            <p>Temperature: ${data.current.temperature}°C</p>
+            <p>Temperature: ${data.current.temperature}°F</p>
             <p>Weather: ${data.current.weather_descriptions[0]}</p>
             <img src="${data.current.weather_icons[0]}" alt="Weather icon">
         `;
